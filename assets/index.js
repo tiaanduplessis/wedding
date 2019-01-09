58d346334b0b7d9beba7db59d5d35da9d3214018
Tiaan du Plessis <tiaanduplessis@hotmail.com>
2019-01-09 14:01:42 +0200

    Updates

--- assets/index.js
+++ assets/index.js
@@ -1,1 +1,179 @@
-"use strict";!function(i){var e=i(".js-counter"),t=e.attr("data-date");e.countdown(t,function(e){i(".js-counter-days").html(e.strftime("%D")),i(".js-counter-hours").html(e.strftime("%H")),i(".js-counter-minutes").html(e.strftime("%M")),i(".js-counter-seconds").html(e.strftime("%S"))}),AOS.init({disable:!1,duration:900,easing:"ease",once:!0});var o=i(".grid").masonry({itemSelector:".grid-item",gutter:".gutter-sizer"});o.imagesLoaded().progress(function(){o.masonry("layout")}),i(document).on("click",".js-open-menu",function(e){e.preventDefault();var t=i(this),o=i(".icon",t),n="menu"===o.attr("name")?"close":"menu";o.attr("name",n),i(".js-menu").slideToggle("slow"),o.toggleClass("icon--black")});var n,a,r,s,l=c();function c(){var e=i(".js-section"),t=[];return e.each(function(){var e=i(this);t.push({id:e.attr("id"),offset:e.offset().top-100})}),t}i(window).on("resize",function(){l=c()}),i(document).on("click",".js-nav-link",function(e){e.preventDefault();var t=i(this).attr("href");i("html, body").animate({scrollTop:i(t).offset().top-100},1e3)}),i(window).on("scroll",(n=function(){for(var e=i(window).scrollTop()+50,t=0;t<l.length;t++)e>=l[t].offset&&(i(".js-nav-link").removeClass("active"),i('.js-nav-link[href="#'+l[t].id+'"]').addClass("active"))},a=50,s=function(){r=void 0,n.call()},function(){r&&clearTimeout(r),r=setTimeout(s,a)})),i(".js-form").on("submit",function(e){e.preventDefault(),i(".js-loader").show();var t=i(this),o=t.attr("action");fetch(o,{method:"POST",body:new URLSearchParams(t.serialize())}).then(function(e){if(200===e.status)return i(".js-loader").hide(),i(".js-form-wrapper").hide(),void i(".js-form-confirmation").show();toasty("Oops! Ons kon nie die RSVP verwerk nie. Probeer asb weer.",6e3)}).catch(function(e){toasty("Oops! Ons kon nie die RSVP verwerk nie. Probeer asb weer.",6e3)})}),"?enkel"===document.location.search&&i(".js-guest-input").attr("disabled",!0),window.initMap=function(){var e={lat:-33.7295,lng:19.0276},t={zoom:14,center:e,mapTypeControl:!1,scrollwheel:!0,draggable:!0,mapTypeControlOptions:{style:google.maps.MapTypeControlStyle.DROPDOWN_MENU},navigationControl:!1,navigationControlOptions:{style:google.maps.NavigationControlStyle.SMALL},mapTypeId:google.maps.MapTypeId.ROADMAP},o=new window.google.maps.Map(document.getElementById("map"),t);new google.maps.Marker({position:e,map:o});google.maps.event.addDomListener(window,"resize",function(){var e=o.getCenter();google.maps.event.trigger(o,"resize"),o.setCenter(e)})}}($);
+'use strict';
+
+/* eslint-env jquery, browser */
+/* global google, AOS, toasty */
+
+(function ($) {
+  var $counter = $('.js-counter')
+  var $date = $counter.attr('data-date')
+
+  $counter.countdown($date, function (event) {
+    $('.js-counter-days').html(event.strftime('%D'))
+    $('.js-counter-hours').html(event.strftime('%H'))
+    $('.js-counter-minutes').html(event.strftime('%M'))
+    $('.js-counter-seconds').html(event.strftime('%S'))
+  })
+
+  AOS.init({
+    disable: false,
+    duration: 900, 
+    easing: 'ease',
+    once: true
+  })
+
+
+  var $grid = $('.grid').masonry({
+    itemSelector: '.grid-item',
+    gutter: '.gutter-sizer'
+  })
+
+  $grid.imagesLoaded().progress(function () {
+    $grid.masonry('layout')
+  })
+
+  $(document).on('click', '.js-open-menu', function (e) {
+    e.preventDefault()
+    var $self = $(this)
+    var $icon = $('.icon', $self)
+    var iconName = $icon.attr('name')
+
+    var iconAttr = iconName === 'menu' ? 'close' : 'menu'
+    $icon.attr('name', iconAttr)
+    $('.js-menu').slideToggle('slow')
+    $icon.toggleClass('icon--black')
+  })
+
+  var sectionsInfo = getSectionsOffset()
+  $(window).on('resize', function () {
+    sectionsInfo = getSectionsOffset()
+  })
+
+  $(document).on('click', '.js-nav-link', function (e) {
+    e.preventDefault()
+
+    var target = $(this).attr('href')
+
+    $('html, body').animate(
+      {
+        scrollTop: $(target).offset().top - 100
+      },
+      1000
+    )
+  })
+
+  $(window).on('scroll', debounce(setActiveNavLink, 50))
+
+  $('.js-form').on('submit', function (e) {
+    e.preventDefault()
+    $('.js-loader').show()
+    var $form = $(this)
+    var action = $form.attr('action')
+
+    fetch(action, {
+      method: 'POST',
+      body: new URLSearchParams($form.serialize())
+    })
+      .then(function (res) {
+        if (res.status === 200) {
+          $('.js-loader').hide()
+          $('.js-form-wrapper').hide()
+          $('.js-form-confirmation').show()
+          return
+        }
+
+        toasty(
+          'Oops! Ons kon nie die RSVP verwerk nie. Probeer asb weer.',
+          6000
+        )
+      })
+      .catch(function (error) {
+        toasty(
+          'Oops! Ons kon nie die RSVP verwerk nie. Probeer asb weer.',
+          6000
+        )
+      })
+  })
+
+  function getSectionsOffset () {
+    var sections = $('.js-section')
+    var sectionsInfo = []
+
+    sections.each(function () {
+      var $self = $(this)
+      sectionsInfo.push({
+        id: $self.attr('id'),
+        offset: $self.offset().top - 100
+      })
+    })
+
+    return sectionsInfo
+  }
+
+  function setActiveNavLink () {
+    var scrollPosition = $(window).scrollTop() + 50
+    for (var i = 0; i < sectionsInfo.length; i++) {
+      if (scrollPosition >= sectionsInfo[i].offset) {
+        $('.js-nav-link').removeClass('active')
+        $('.js-nav-link[href="#' + sectionsInfo[i].id + '"]').addClass(
+          'active'
+        )
+      }
+    }
+  }
+
+  
+  function debounce (func, wait) {
+    var timeout
+    var later = function later () {
+      timeout = undefined
+      func.call()
+    }
+
+    return function () {
+      if (timeout) {
+        clearTimeout(timeout)
+      }
+      timeout = setTimeout(later, wait)
+    }
+  }
+
+  if (document.location.search === '?enkel') {
+    $('.js-guest-input').attr('disabled', true)
+  }
+
+  window.initMap = function initMap () {
+    // Venue location
+    var latlng = { lat: -33.7295, lng: 19.0276 }
+
+    var settings = {
+      zoom: 14,
+      center: latlng,
+      mapTypeControl: false,
+      scrollwheel: true,
+      draggable: true,
+      mapTypeControlOptions: {
+        style: google.maps.MapTypeControlStyle.DROPDOWN_MENU
+      },
+      navigationControl: false,
+      navigationControlOptions: {
+        style: google.maps.NavigationControlStyle.SMALL
+      },
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+
+    var map = new window.google.maps.Map(
+      document.getElementById('map'),
+      settings
+    )
+
+    var marker = new google.maps.Marker({ position: latlng, map: map })
+
+    google.maps.event.addDomListener(window, 'resize', handleResize)
+
+    function handleResize () {
+      var center = map.getCenter()
+      google.maps.event.trigger(map, 'resize')
+      map.setCenter(center)
+    }
+  }
+})($)
